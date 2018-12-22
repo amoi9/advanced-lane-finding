28ee5baf3f496b30308dd4c70e3fb13a8db7275d
amoi9 <aileenzhang7+github@gmail.com>
2018-12-21 16:20:17 -0800

    init process

--- code/curvature_and_offset.py
+++ code/curvature_and_offset.py
@@ -1,33 +1,42 @@
 import numpy as np
 from lane_pixel_finder import find_lane_pixels
 
-def measure_curvature_and_offset_real(binary_warped):
-    '''
-    Calculates the curvature of polynomial functions in meters.
-    '''
-    # Define conversions in x and y from pixels space to meters
-    ym_per_pix = 30/720 # meters per pixel in y dimension
-    xm_per_pix = 3.7/700 # meters per pixel in x dimension
-    
-    # Find our lane pixels first
-    leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
+'''
+Calculates the curvature of polynomial functions in meters.
+'''
+# Define conversions in x and y from pixels space to meters
+ym_per_pix = 30/720 # meters per pixel in y dimension
+xm_per_pix = 3.7/700 # meters per pixel in x dimension
 
+def measure_curvature_and_offset_real(binary_warped, direction='left'):
+    # Find our lane pixels first
+    leftx, lefty, rightx, righty, _ = find_lane_pixels(binary_warped)
+    if direction == 'left':
+        x = leftx
+        y = lefty
+    else:
+        x = rightx
+        y = righty
+    curature = measure_curvature_real_with_pixels(binary_warped.shape[1], binary_warped.shape[0], x, y, direction)
+    
+    offset_pix = (leftx[0] + rightx[0]) / 2 - binary_warped_width / 2
+    offset = offset_pix * xm_per_pix
+    return curature, offset
+
+def measure_curvature_real_with_pixels(binary_warped_width, binary_warped_length, x, y):
     # Generate x and y values for plotting
-    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    ploty = np.linspace(0, binary_warped_length-1, binary_warped_length)
     
     # Fit a second order polynomial to each using `np.polyfit`
-    left_fit_cr = np.polyfit(lefty*ym_per_pix, leftx*xm_per_pix, 2)
-#     right_fit_cr = np.polyfit(righty*ym_per_pix, rightx*xm_per_pix, 2)
+    fit_cr = np.polyfit(y*ym_per_pix, x*xm_per_pix, 2)
     
     # Define y-value where we want radius of curvature
     # We'll choose the maximum y-value, corresponding to the bottom of the image
     y_eval = np.max(ploty)
     
     ##### calculation of R_curve (radius of curvature) #####
-    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
-#     right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
-    
-    offset_pix = (leftx[0] + rightx[0]) / 2 - binary_warped.shape[1] / 2
-    offset = offset_pix * xm_per_pix
-    
-    return left_curverad, offset
+    curverad = ((1 + (2*fit_cr[0]*y_eval*ym_per_pix + fit_cr[1])**2)**1.5) / np.absolute(2*fit_cr[0])
+    return curverad
+
+def measure_offset_real(binary_warped_width, x):
+    return (x[0] - binary_warped_width / 2) * xm_per_pix
