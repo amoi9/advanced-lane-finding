19a521a93c8e1ffa5fa6c933d61521c46888a72b
amoi9 <aileenzhang7+github@gmail.com>
2018-12-22 22:10:17 -0800

    search around only;

--- code/fit_polynomial.py
+++ code/fit_polynomial.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-def fit_polynomial(binary_warped, leftx, lefty, rightx, righty):
+def fit_polynomial(img_shape, leftx, lefty, rightx, righty):
     # Fit a second order polynomial to each using `np.polyfit`
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
 
     # Generate x and y values for plotting
-    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    ploty = np.linspace(0, img_shape[0]-1, img_shape[0] )
     try:
         left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
         right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
